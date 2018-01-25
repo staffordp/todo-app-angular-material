6bd537fa40d8df2ea583c63cb1a0120578294fcf
Phillip Stafford <phillip@philliprstafford.com>
2018-01-25 17:09:08 -0500

    Rename AppModule to TodoAppModule

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
+import { TodoAppModule } from './app/app.module';
 import { environment } from './environments/environment';
 import 'hammerjs';
 
@@ -9,5 +9,5 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+platformBrowserDynamic().bootstrapModule(TodoAppModule)
   .catch(err => console.log(err));
