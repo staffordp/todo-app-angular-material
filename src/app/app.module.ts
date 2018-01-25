737f565d1d20caa6eae63fff0527a1ea82061aa7
Phillip Stafford <phillip@philliprstafford.com>
2018-01-25 17:17:11 -0500

    Create header component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
