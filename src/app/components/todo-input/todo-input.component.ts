021ce49754e661038547323d5a473d36d6b1bdfc
grimal <phillip@philliprstafford.com>
2018-01-26 11:41:15 -0500

    Remove providers array since empty. Add constructor which instantiates a local TodoService as todoService. In constructor, set this.todoText equal to empty string.

--- src/app/components/todo-input/todo-input.component.ts
+++ src/app/components/todo-input/todo-input.component.ts
@@ -3,17 +3,19 @@ import {FormControl} from '@angular/forms';
 import * as _moment from 'moment';
 import {default as _rollupMoment} from 'moment';
 const moment = _rollupMoment || _moment;
+import { TodoService } from '../../services/todo.service';
 
 @Component({
   selector: 'app-todo-input',
   templateUrl: './todo-input.component.html',
-  styleUrls: ['./todo-input.component.css'],
-  providers: [
-  ],
+  styleUrls: ['./todo-input.component.css']
 })
 export class TodoInputComponent implements OnInit {
 
-  constructor() {
+  private todoText: string;
+
+  constructor(private todoService: TodoService) {
+    this.todoText = '';
   }
 
   ngOnInit() {
