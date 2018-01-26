590aaf555e0d7da387a8fd4edfc94cd299ba35e0
grimal <phillip@philliprstafford.com>
2018-01-26 11:05:43 -0500

    Add date picker to component and import dependencies.

--- src/app/components/todo-input/todo-input.component.ts
+++ src/app/components/todo-input/todo-input.component.ts
@@ -1,13 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl} from '@angular/forms';
+import * as _moment from 'moment';
+import {default as _rollupMoment} from 'moment';
+const moment = _rollupMoment || _moment;
 
 @Component({
   selector: 'app-todo-input',
   templateUrl: './todo-input.component.html',
-  styleUrls: ['./todo-input.component.css']
+  styleUrls: ['./todo-input.component.css'],
+  providers: [
+  ],
 })
 export class TodoInputComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
