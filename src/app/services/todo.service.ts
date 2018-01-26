2e2e2973d3e613b5c91b3bdb815c0bf0aeda63ec
grimal <phillip@philliprstafford.com>
2018-01-26 12:37:49 -0500

    Add function in service to add todos. First, let todo variable equal a new Todo with the following parameters: nextIndex, text, date. After, push the new todo to the array. After, log the array.

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -4,11 +4,11 @@ import { Todo } from '../classes/todo';
 @Injectable()
 export class TodoService {
 
-  private todosArr: Todo[];
+  private todoArr: Todo[];
   private nextIndex: number;
 
   constructor() {
-    this.todosArr = [
+    this.todoArr = [
       new Todo(0, 'blank text'),
 
     ]
@@ -16,7 +16,17 @@ export class TodoService {
   }
 
   public getTodos(): Todo[] {
-    return this.todosArr;
+    return this.todoArr;
+  }
+
+  public addTodo(text: string, date: string): void {
+    console.log('Adding todo!');
+    // Create a new todo from the input
+    let todo = new Todo(this.nextIndex, text, date);
+    // Push the todo to the array
+    this.todoArr.push(todo);
+    console.log('Todos are:');
+    console.log(this.todoArr);
   }
 
 }
