628809867cf075cf311f3bd4b0160388bdb9793b
Phillip Stafford <phillip@philliprstafford.com>
2018-01-26 14:08:12 -0500

    Import moment. Add constructor lines from starter moment install guid.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
-
+import * as moment from 'moment';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'todoApp';
+  constructor() {
+    let now = moment(); // add this 2 of 4
+    console.log('hello world', now.format());
+    console.log(now.add(7, 'days').format());
+  }
 }
