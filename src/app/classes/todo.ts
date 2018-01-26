f71f4a213dfc7b4d4e0072a317fef83531236ea8
grimal <phillip@philliprstafford.com>
2018-01-26 12:35:35 -0500

    Add date field to class.  Set as type string.

--- src/app/classes/todo.ts
+++ src/app/classes/todo.ts
@@ -1,9 +1,11 @@
 export class Todo {
   index: number;
   text: string;
+  date: string;
 
-  constructor(index: number, text: string) {
+  constructor(index: number, text: string, date: string) {
     this.index = index;
+    this.date = date;
     this.text = text;
   }
 }
