1e88ade039de686ae5bc5d2fe158ae107bbbce2f
Phillip Stafford <phillip@philliprstafford.com>
2018-01-26 17:06:24 -0500

    Import TodoService and Input module. Import todo clsss. @input, set the private variable todo to be an instance of Todo class. set Contstructor to import TodoService.

--- src/app/components/todo-item/todo-item.component.ts
+++ src/app/components/todo-item/todo-item.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { TodoService } from '../../services/todo.service';
+import { Todo } from '../../classes/todo';
 
 @Component({
   selector: 'app-todo-item',
@@ -7,7 +9,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoItemComponent implements OnInit {
 
-  constructor() { }
+  @Input()
+
+  private todo: Todo;
+  constructor(private todoService: TodoService) { }
 
   ngOnInit() {
   }
