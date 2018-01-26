b9bd327d4fba320c26b0404beb42e39a12786652
Phillip Stafford <phillip@philliprstafford.com>
2018-01-26 17:29:41 -0500

    Swap order of variables in constructor.

--- src/app/classes/todo.ts
+++ src/app/classes/todo.ts
@@ -3,9 +3,10 @@ export class Todo {
   text: string;
   date: string;
 
-  constructor(index: number, text: string, date: string) {
+  constructor(index: number, date: string, text: string) {
     this.index = index;
     this.date = date;
     this.text = text;
+
   }
 }
