c5d12823768b63112e4c566c70bcbbf8f1d78915
Phillip Stafford <phillip@philliprstafford.com>
2018-01-27 16:04:23 -0500

    Add todo-wrapper to app.

---
+++ src/app/components/todo-wrapper/todo-wrapper.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TodoWrapperComponent } from './todo-wrapper.component';
+
+describe('TodoWrapperComponent', () => {
+  let component: TodoWrapperComponent;
+  let fixture: ComponentFixture<TodoWrapperComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ TodoWrapperComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TodoWrapperComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/todo-wrapper/todo-wrapper.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { TodoService } from '../../services/todo.service';
+
+
+@Component({
+  selector: 'app-todo-wrapper',
+  templateUrl: './todo-wrapper.component.html',
+  styleUrls: ['./todo-wrapper.component.css']
+})
+export class TodoWrapperComponent implements OnInit {
+  @Input()
+  private showingProfile: boolean;
+
+  constructor(private todoService: TodoService) {
+    this.showingProfile = false;
+
+  }
+
+  ngOnInit() {
+  }
+
+}
