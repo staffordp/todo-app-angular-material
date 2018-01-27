e43a060f0100a6ef99fe5efc996f9c2334812f81
Phillip Stafford <phillip@philliprstafford.com>
2018-01-27 18:40:00 -0500

    Uncomment out lines. Delete old lines.

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -11,17 +11,10 @@ export class ProfileComponent implements OnInit {
   // private showingProfile: boolean;
 
   constructor(private todoService: TodoService) {
-    this.userName = 'Guest';
-    // this.showingProfile = false;
+    this.userName = todoService.getProfile();
   }
 
   private userName: string;
-
-  // private showProfile():void {
-  //   console.log('Am showing profile now.');
-  //   this.todoService.setShowprofile();
-  // }
-
 
 
   ngOnInit() {
