48779219512a41f61a7d6fa93bb8168ed94cdc65
grimal <phillip@philliprstafford.com>
2018-01-28 11:56:36 -0500

    Comment out console.log lines. In cancelTodo function, reset the todoText.

--- src/app/components/todo-input/todo-input.component.ts
+++ src/app/components/todo-input/todo-input.component.ts
@@ -17,37 +17,31 @@ export class TodoInputComponent implements OnInit {
   private showTodoinput: boolean;
   serializedDate = new FormControl((new Date()).toISOString());
 
-
-
   constructor(private todoService: TodoService) {
     this.todoText = '';
     this.todoDate = this.serializedDate.value;
     this.showTodoinput = todoService.getShowinput();
     this.showMe = false;
-    console.log(this.showTodoinput);
+    // console.log(this.showTodoinput);
   }
 
   ngOnInit() {
   }
 
   private addTodo(): void {
-    console.log('Adding TODO.');
-    console.log(this.todoDate);
-    console.log(typeof(this.todoDate));
-
+    // console.log('Adding TODO.');
+    // console.log(this.todoDate);
+    // console.log(typeof(this.todoDate));
     this.todoService.addTodo(this.todoDate, this.todoText);
     // Reset totoText after submission
     this.todoText = '';
-
-
   }
 
   private cancelTodo(): void {
-    console.log('Csncelling todo');
+    // console.log('Csncelling todo');
     this.showMe = false;
     this.todoService.showInput();
-
-
+    this.todoText = '';
   }
 
 }
